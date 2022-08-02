be15c39320fee2ff8f7a2226c812b585019c45ce
Sipho123 <58542277+Sipho123@users.noreply.github.com>
2022-08-02 03:17:46 +0300

    fourth commit

--- our_store/urls.py
+++ our_store/urls.py
@@ -13,5 +13,6 @@ app_name = 'our_store'
 urlpatterns = [
         path('store/', StoreView.as_view(), name='store'),
         path('all-products/', AllProductsView.as_view(), name='allproducts'),
+        path('products/<slug:slug>/', ProductDetailView.as_view(), name='productdetail'),
 
 ]
--- our_store/views.py
+++ our_store/views.py
@@ -21,5 +21,18 @@ class AllProductsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['allcategories'] = Categories.objects.all()
+        context['allcategories'] = Category.objects.all()
         return  context
+
+
+class ProductDetailView(TemplateView):
+    template_name ="productdetail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        product = Product.objects.get[slug=url_slug]
+        product.view count += 1
+        product.save()
+        context['product'] = product
+        return  context       
